056393018909300bbf366db6093f3228fde9114d
TheTrainCrazyMan <thatt196@gmail.com>
2019-09-12 09:28:31 -0400

    Create javascriptbutton.js
    Ongoing tests

---
+++ tests/javascriptbutton.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Show a push button:</h1>
+
+<p>The button below activates a JavaScript when it is clicked.</p>
+
+<form>
+  <input type="button" value="Click me" onclick="msg()">
+</form>
+
+<script>
+function msg() {
+  alert("Hello world!");
+}
+</script>
+
+</body>
+</html>
